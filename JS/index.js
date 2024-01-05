6f0641ca5004699a3acb51356409d7d39cfd0161
GustavoPerpetua <gustavoaperpe@gmail.com>
2024-01-05 18:52:34 -0300

    1st js program

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+console.log("hola mundo")
+
+// Tipos de datos en JS
+// String: cadena de caracteres.
+// Boolean: true false.
+// Null: nulo.
+// Number: 1234 1 34 44
+// "123" entre "" es un string != 123
+// Undefined: no definido.
+// Object: objeto. puede tener cualquier combinacion de datos
